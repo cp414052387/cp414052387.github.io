9578916a2bf13e2624ba7504eec03177e2304dd3
“cp414052387” <chenpeng0738@sina.com>
2025-02-11 21:56:00 +0800

    Site updated: 2025-02-11 21:56:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/08/hexo-blog美化/","1970/01/01/PMP学习第二课-价值交付系统/","2025/02/10/PMP学习第一课-引论/","2025/02/03/第一课-如何搭建blog/","2025/02/02/hello-world/"];function toRandomPost(){
+var posts=["2025/02/11/PMP学习第二课-价值交付系统/","2025/02/08/hexo-blog美化/","2025/02/10/PMP学习第一课-引论/","2025/02/03/第一课-如何搭建blog/","2025/02/02/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
